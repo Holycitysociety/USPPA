34271f46c73087da086b8b8956dbbff8e24e684f
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2025-08-16 12:14:04 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder script file
+// For now, no interactivity needed, but this is where
+// animations, scroll effects, or analytics hooks can be added later.
+
+console.log("US Polo Patrons Association site loaded");
