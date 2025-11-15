e9da30b306d64fba55c53596f622dcdc6784aa9b
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2025-11-15 15:14:33 -0500

    Implement PoloPatronium contract
    Added PoloPatronium contract inheriting from ERC1967Proxy.

--- PoloPatronium.sol
+++ PoloPatronium.sol
@@ -1,1 +1,21 @@
-.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract PoloPatronium is ERC1967Proxy {
+    constructor(
+        address implementation,
+        address[] memory operators,
+        address owner_
+    )
+        ERC1967Proxy(
+            implementation,
+            abi.encodeWithSignature(
+                "initialize(address[],address)",
+                operators,
+                owner_
+            )
+        )
+    {}
+}
